1e5ec6f04ec2860d6ec0b952933224839eb98ab6
codertinie <martin.munyao@moringaschool.com>
2022-11-26 14:44:31 +0300

    commit tree 9036d581826e820c8900908a25270a0ab04d37ed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,8 @@
 class BirdsController < ApplicationController
+ 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # added rescue_from
+  
 
   # GET /birds
   def index
@@ -9,7 +12,8 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
 
@@ -22,8 +26,10 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   # DELETE /birds/:id
@@ -34,6 +40,10 @@ class BirdsController < ApplicationController
   end
 
   private
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 
   def find_bird
     Bird.find(params[:id])
